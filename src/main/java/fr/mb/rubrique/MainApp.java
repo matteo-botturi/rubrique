be9e6261c6ed8cf23571a395eb4e2baaf5a0db93
Matteo <botturi.matteo@gmail.com>
2024-10-23 16:10:19 +0200

    Added listener to table, class Outil and filling fields.

--- src/main/java/fr/mb/rubrique/MainApp.java
+++ src/main/java/fr/mb/rubrique/MainApp.java
@@ -1,25 +1,56 @@
 package fr.mb.rubrique;
 
 import java.io.IOException;
+import fr.mb.rubrique.model.Person;
+import fr.mb.rubrique.view.PersonOverviewController;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import lombok.Getter;
 
 public class MainApp extends Application {
 
-	@Getter
 	private Stage primaryStage;
 	private BorderPane rootLayout;
+	
+	/**
+	 * The data as an observable list of Persons.
+	 */
+	private ObservableList<Person> personData = FXCollections.observableArrayList();
+	
+	/**
+	 * Constructor
+	 */
+	public MainApp() {
+		// Add some sample data
+		personData.add(new Person("Hans", "Muster"));
+		personData.add(new Person("Ruth", "Mueller"));
+		personData.add(new Person("Heinz", "Kurz"));
+		personData.add(new Person("Cornelia", "Meier"));
+		personData.add(new Person("Werner", "Meyer"));
+		personData.add(new Person("Lydia", "Kunz"));
+		personData.add(new Person("Anna", "Best"));
+		personData.add(new Person("Stefan", "Meier"));
+		personData.add(new Person("Martin", "Mueller"));
+	}
+	
+	/**
+	 * Returns the data as an observable list of Persons. 
+	 * @return
+	 */
+	public ObservableList<Person> getPersonData() {
+		return personData;
+	}
 
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("AddressApp");
-
+		this.primaryStage.setTitle("Rubrique de contacts");
+		
 		initRootLayout();
 		showPersonOverview();
 	}
@@ -55,9 +86,21 @@ public class MainApp extends Application {
 
 			// Set person overview into the center of root layout.
 			rootLayout.setCenter(personOverview);
+			
+			// Give the controller access to the main app.
+	        PersonOverviewController controller = loader.getController();
+	        controller.setMainApp(this);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Returns the main stage.
+	 * @return
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
 	}
 
 	public static void main(String[] args) {
